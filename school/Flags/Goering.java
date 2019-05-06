4daaa7bdcda56a9596f0bacd7d2bb0aee58698c8
augustadahl <august.adahl@outlook.com>
2019-05-06 08:52:32 +0200

    h

--- school/Flags/Goering.java
+++ school/Flags/Goering.java
@@ -10,11 +10,11 @@ public class Goering extends Application{
 
 	public void start(Stage primaryStage) throws Exception {
 		Group root = new Group();
-		Scene scene = new Scene(root,500,500,Color.SKYBLUE);
+		Scene scene = new Scene(root,800,600,Color.SKYBLUE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		Nasse flag = new Nasse(500, 1);
+		Nasse flag = new Nasse(300, 1);
 		flag.setTranslateX(primaryStage.getWidth()/2);
 		flag.setTranslateY(primaryStage.getHeight()/2);
 		root.getChildren().add(flag);
